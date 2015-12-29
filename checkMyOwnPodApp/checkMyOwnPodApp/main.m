85bb830cbab279519cdc73af0ff7deb09172ea21
Shani <shani@quickode.com>
2015-12-29 17:38:17 +0200

    first commit

---
+++ checkMyOwnPodApp/checkMyOwnPodApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  checkMyOwnPodApp
+//
+//  Created by Shani Quickode on 12/29/15.
+//  Copyright © 2015 Quickode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
